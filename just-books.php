da2af441d337ef0fe8686fbf133e959cdfc99492
zarei.dev <mohammad.zarei1380@gmail.com>
2021-10-14 01:05:17 +0330

    first init ver 0.0.9

---
+++ just-books.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *
+ * Plugin Name:       Just Books
+ * Plugin URI:        https://moza.dev/plugins/just-books/
+ * Description:       books by Mohammad Zarei
+ * Version:           0.0.9
+ * Author:            Mohammad Zarei
+ * Author URI:        https://moza.dev/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       just-books
+ * Domain Path:       /languages
+ */
+
+defined('ABSPATH') || exit;
+
+
+require __DIR__ . '/includes/Autoloader.php';
